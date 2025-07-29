22dbc9243ce015ff502384110e2234a5fd91b893
Liam Degand <degandliam013@gmail.com>
2025-07-28 19:37:15 -0700

    Updated variables for RX with the help of Abdul

--- flowgraphs/rx_flowgraph.py
+++ flowgraphs/rx_flowgraph.py
@@ -10,9 +10,10 @@
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from PyQt5.QtCore import QObject, pyqtSlot
-from gnuradio import analog
 from gnuradio import blocks
+import pmt
+from gnuradio import blocks, gr
+from gnuradio import digital
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
@@ -22,7 +23,10 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import sip
+from gnuradio import gr, pdu
+from gnuradio import network
+from gnuradio import soapy
+import satellites
 import threading
 
 
@@ -63,132 +67,82 @@ class rx_flowgraph(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32000
-        self.frequency = frequency = 0
+        self.threshold = threshold = 450
+        self.tcp_port = tcp_port = '52001'
+        self.sync_word = sync_word = 1
+        self.samp_rate = samp_rate = 30.72M
+        self.preamble = preamble = 0b10101010
+        self.payload_len = payload_len = 128
+        self.host_ip = host_ip = '127.0.0.1'
+        self.frame_len = frame_len = 450
+        self.crc_poly = crc_poly = 0x8005
+        self.center_freq = center_freq = 915e6
+        self.RF_gain = RF_gain = 30
+        self.K = K = 8
+        self.Bandwith = Bandwith = 30.72M
 
         ##################################################
         # Blocks
         ##################################################
 
-        # Create the options list
-        self._frequency_options = [0, 1000, -2000]
-        # Create the labels list
-        self._frequency_labels = ['Frequency: 0', 'Frequency: 1000', 'Frequency: -2000']
-        # Create the combo box
-        self._frequency_tool_bar = Qt.QToolBar(self)
-        self._frequency_tool_bar.addWidget(Qt.QLabel("'frequency'" + ": "))
-        self._frequency_combo_box = Qt.QComboBox()
-        self._frequency_tool_bar.addWidget(self._frequency_combo_box)
-        for _label in self._frequency_labels: self._frequency_combo_box.addItem(_label)
-        self._frequency_callback = lambda i: Qt.QMetaObject.invokeMethod(self._frequency_combo_box, "setCurrentIndex", Qt.Q_ARG("int", self._frequency_options.index(i)))
-        self._frequency_callback(self.frequency)
-        self._frequency_combo_box.currentIndexChanged.connect(
-            lambda i: self.set_frequency(self._frequency_options[i]))
-        # Create the radio buttons
-        self.top_layout.addWidget(self._frequency_tool_bar)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
-            1024, #size
-            samp_rate, #samp_rate
-            "", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        self.soapy_limesdr_source_0 = None
+        dev = 'driver=lime'
+        stream_args = ''
+        tune_args = ['']
+        settings = ['']
 
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
-
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
-
-
-        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
-            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ['blue', 'red', 'green', 'black', 'cyan',
-            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1]
-
-
-        for i in range(2):
-            if len(labels[i]) == 0:
-                if (i % 2 == 0):
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
-                else:
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
-            else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
-            1024, #size
-            window.WIN_BLACKMAN_hARRIS, #wintype
-            0, #fc
-            samp_rate, #bw
-            "", #name
-            1,
-            None # parent
-        )
-        self.qtgui_freq_sink_x_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0.set_y_axis((-140), 10)
-        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0.enable_grid(False)
-        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0.enable_control_panel(False)
-        self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
-
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, frequency, 1, 0, 0)
+        self.soapy_limesdr_source_0 = soapy.source(dev, "fc32", 1, 'driver=lime',
+                                  stream_args, tune_args, settings)
+        self.soapy_limesdr_source_0.set_sample_rate(0, samp_rate)
+        self.soapy_limesdr_source_0.set_bandwidth(0, Bandwith)
+        self.soapy_limesdr_source_0.set_frequency(0, center_freq)
+        self.soapy_limesdr_source_0.set_frequency_correction(0, 0)
+        self.soapy_limesdr_source_0.set_gain(0, min(max(RF_gain, -12.0), 61.0))
+        self.satellites_crc_check_0 = satellites.crc_check(16, crc_poly, 0xFFFFFFFF, 0xFFFFFFFF, True, True, False, False, 0)
+        self.pdu_tagged_stream_to_pdu_0 = pdu.tagged_stream_to_pdu(gr.types.byte_t, 'packet_len')
+        self.pdu_pdu_to_tagged_stream_0 = pdu.pdu_to_tagged_stream(gr.types.byte_t, 'packet_len')
+        self.network_socket_pdu_0_0 = network.socket_pdu('TCP_SERVER', host_ip, tcp_port, 10000, False)
+        self.network_socket_pdu_0 = network.socket_pdu('TCP_SERVER', '', '52001', 1500, False)
+        self.digital_gfsk_demod_0 = digital.gfsk_demod(
+            samples_per_symbol=4,
+            sensitivity=1,
+            gain_mu=0.175,
+            mu=0.5,
+            omega_relative_limit=0.005,
+            freq_error=0.0,
+            verbose=False,
+            log=True)
+        self.digital_correlate_access_code_xx_ts_0 = digital.correlate_access_code_bb_ts('10101010',
+          threshold, 'packet_len')
+        self.blocks_unpack_k_bits_bb_0 = blocks.unpack_k_bits_bb(K)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_char*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_tagged_stream_align_0 = blocks.tagged_stream_align(gr.sizeof_char*1, 'packet_len')
+        self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(K)
+        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.cons(pmt.make_dict(), pmt.make_u8vector(4, ord('x')))
+        , 1000)
+        self.blocks_message_debug_0_0 = blocks.message_debug(True, gr.log_levels.err)
+        self.blocks_message_debug_0 = blocks.message_debug(True, gr.log_levels.info)
+        self.Output = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\degan\\OneDrive\\Documents\\Work\\Projects\\UVSD\\Information\\raspiENV\\scripts\\output.txt', False)
+        self.Output.set_unbuffered(True)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.msg_connect((self.blocks_message_strobe_0, 'strobe'), (self.network_socket_pdu_0_0, 'pdus'))
+        self.msg_connect((self.network_socket_pdu_0, 'pdus'), (self.blocks_message_debug_0, 'print_pdu'))
+        self.msg_connect((self.network_socket_pdu_0, 'pdus'), (self.pdu_pdu_to_tagged_stream_0, 'pdus'))
+        self.msg_connect((self.pdu_tagged_stream_to_pdu_0, 'pdus'), (self.satellites_crc_check_0, 'in'))
+        self.msg_connect((self.satellites_crc_check_0, 'fail'), (self.blocks_message_debug_0_0, 'log'))
+        self.msg_connect((self.satellites_crc_check_0, 'ok'), (self.network_socket_pdu_0, 'pdus'))
+        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.blocks_tagged_stream_align_0, 0))
+        self.connect((self.blocks_tagged_stream_align_0, 0), (self.pdu_tagged_stream_to_pdu_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.blocks_unpack_k_bits_bb_0, 0))
+        self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.digital_correlate_access_code_xx_ts_0, 0))
+        self.connect((self.digital_correlate_access_code_xx_ts_0, 0), (self.blocks_pack_k_bits_bb_0, 0))
+        self.connect((self.digital_gfsk_demod_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.pdu_pdu_to_tagged_stream_0, 0), (self.Output, 0))
+        self.connect((self.soapy_limesdr_source_0, 0), (self.digital_gfsk_demod_0, 0))
 
 
     def closeEvent(self, event):
@@ -199,23 +153,88 @@ class rx_flowgraph(gr.top_block, Qt.QWidget):
 
         event.accept()
 
+    def get_threshold(self):
+        return self.threshold
+
+    def set_threshold(self, threshold):
+        self.threshold = threshold
+
+    def get_tcp_port(self):
+        return self.tcp_port
+
+    def set_tcp_port(self, tcp_port):
+        self.tcp_port = tcp_port
+
+    def get_sync_word(self):
+        return self.sync_word
+
+    def set_sync_word(self, sync_word):
+        self.sync_word = sync_word
+
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.soapy_limesdr_source_0.set_sample_rate(0, self.samp_rate)
 
-    def get_frequency(self):
-        return self.frequency
+    def get_preamble(self):
+        return self.preamble
 
-    def set_frequency(self, frequency):
-        self.frequency = frequency
-        self._frequency_callback(self.frequency)
-        self.analog_sig_source_x_0.set_frequency(self.frequency)
+    def set_preamble(self, preamble):
+        self.preamble = preamble
+
+    def get_payload_len(self):
+        return self.payload_len
+
+    def set_payload_len(self, payload_len):
+        self.payload_len = payload_len
+
+    def get_host_ip(self):
+        return self.host_ip
+
+    def set_host_ip(self, host_ip):
+        self.host_ip = host_ip
+
+    def get_frame_len(self):
+        return self.frame_len
+
+    def set_frame_len(self, frame_len):
+        self.frame_len = frame_len
+
+    def get_crc_poly(self):
+        return self.crc_poly
+
+    def set_crc_poly(self, crc_poly):
+        self.crc_poly = crc_poly
+
+    def get_center_freq(self):
+        return self.center_freq
+
+    def set_center_freq(self, center_freq):
+        self.center_freq = center_freq
+        self.soapy_limesdr_source_0.set_frequency(0, self.center_freq)
+
+    def get_RF_gain(self):
+        return self.RF_gain
+
+    def set_RF_gain(self, RF_gain):
+        self.RF_gain = RF_gain
+        self.soapy_limesdr_source_0.set_gain(0, min(max(self.RF_gain, -12.0), 61.0))
+
+    def get_K(self):
+        return self.K
+
+    def set_K(self, K):
+        self.K = K
+
+    def get_Bandwith(self):
+        return self.Bandwith
+
+    def set_Bandwith(self, Bandwith):
+        self.Bandwith = Bandwith
+        self.soapy_limesdr_source_0.set_bandwidth(0, self.Bandwith)
 
 
 
